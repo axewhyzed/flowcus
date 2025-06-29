da53452005eea51e517f043d26fe22ac3468c87e
Mihir Bambhaniya <mihir0234@outlook.com>
2025-06-29 15:24:03 +0530

    fixed issue of prerendering dynamic routes

--- FlowCus-frontend/flowcus/src/app/app.routes.server.ts
+++ FlowCus-frontend/flowcus/src/app/app.routes.server.ts
@@ -2,6 +2,42 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'dashboard',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'tasks',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'templates',
+    renderMode: RenderMode.Prerender
+  },
+
+  // dynamic routes should be client only:
+  {
+    path: 'tasks/edit/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'templates/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'templates/:id/edit',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'templates/:id/items',
+    renderMode: RenderMode.Client
+  },
+
+  // fallback
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
