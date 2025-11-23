e471f0f434924dc53cbffcbfa3433f4aa4918a12
Mihir Bambhaniya <mihir0234@outlook.com>
2025-11-24 00:08:18 +0530

    minor fix

--- FlowCus-backend/Models/TaskSubtype.cs
+++ FlowCus-backend/Models/TaskSubtype.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace FlowCus.Models
 {
@@ -14,7 +15,7 @@ namespace FlowCus.Models
 
         [Column("user_id")]
         public int UserId { get; set; }
-        
+
         [JsonIgnore]
         public virtual User User { get; set; } = null!;
 
